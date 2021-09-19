c803ff549030b4c54de14464ec79e9a0ea64d0d9
Qian Tang <q.tang6@student.unimelb.edu.au>
2021-09-19 22:50:53 +1000

    import vant components library

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
   <div id="app">
     <router-view />
     <h1>News app</h1>
+    <div>
+      <i class="toutiao toutiao-shipin"></i>
+    </div>
+    <van-button type="primary">Primary</van-button>
+    <van-button type="info">Info</van-button>
+    <van-button type="default">Default</van-button>
+    <van-button type="danger">Danger</van-button>
+    <van-button type="warning">Warning</van-button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// import global styles
 import './styles/index.less'
+
+// import vant and its styles
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// reg and use vant
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
